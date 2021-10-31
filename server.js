d357581c6abcdc9c3d03f02be4ba632bac2bb0f4
abdulkalam7 <77677121+abdulkalam7@users.noreply.github.com>
2021-10-31 13:36:39 +0530

    http

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const morgan = require("morgan");
 const app = express();
 dotenv.config();
 //socket
-const http = require("https").createServer(app);
+const http = require("http").createServer(app);
 const io = require("socket.io")(http,{
     path:'/socket.io',
     cors:{
-        origin:"https://abdul-blogger-app.netlify.app",
+        origin:"https://abdul-blogger-app.netlify.app/",
         methods:["GET","POST"],
         allowedHeaders:["Content-type"],
     },
